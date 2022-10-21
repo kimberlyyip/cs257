615a864b26d82d6c6da849e2ffd31564b514ba30
Kimberly Yip <kimberlyyip@Kimberlys-MacBook-Pro.local>
2022-10-21 09:28:53 -0500

    initial commit

---
+++ http_apis_labs/covid_api_test.py
@@ -0,0 +1,15 @@
+'''
+covid_api_test.py
+
+A program to retrieve results from an HTTP-based API, parse the results (JSON in this case), and manage the potential errors.
+'''
+
+import sys
+import argparse
+import json
+import urllib.request
+
+API_BASE_URL = 'https://api.covidtracking.com'
+
+def get_state_cases():
+    pass
