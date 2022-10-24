a9ff5f1edb6cdbbcc35850ad3845507cd2ce5b19
Kimberly Yip <kimberlyyip@Kimberlys-MacBook-Pro.local>
2022-10-24 09:40:44 -0500

    more code

--- http_apis_labs/covid_api_test.py
+++ http_apis_labs/covid_api_test.py
@@ -12,5 +12,11 @@ import urllib.request
 API_BASE_URL = 'https://api.covidtracking.com'
 
 def get_state_cases(state):
-    url = f'{API_BASE_URL}'
-    #/v1/states/current.json
+    url = f'{API_BASE_URL}/v1/states/{state}/current.json'
+    data_from_server = urllib.request.urlopen(url).read()
+    string_from_server = data_from_server.decode('utf-8')
+    state_case_list = json.load(string_from_server)
+    result_list = []
+    for case_dictionary in state_case_list:
+        date = case_dictionary.get('date', '')
+        positive_cases = 
