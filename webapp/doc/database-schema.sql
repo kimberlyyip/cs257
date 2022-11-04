a26e843360f7adb1d5c6bb64efd103bb8c46f23d
Kimberly Yip <kimberlyyip@Kimberlys-MacBook-Pro.local>
2022-11-04 09:14:42 -0500

    "formatting"

--- webapp/doc/database-schema.sql
+++ webapp/doc/database-schema.sql
@@ -5,53 +5,52 @@ Authors: Sydney Nguyen, Sophia Wang, Kimberly Yip
 
 
 CREATE TABLE user(
-	Id_user,
-	username,
-	list of favorite game ids,
-password 
+	id SERIAL,
+	username text,
+	list_of_favorite_games_ids integer,
+	password text
 );
 
 CREATE TABLE all_users(
-	id_user
+	id_user SERIAL
 );
 
 CREATE TABLE review(
-	id_user,
-	text
+	id_user SERIAL,
+	review text,
+	id_board_game integer
 );
 
 CREATE TABLE board_game(
-	id board_game,
-	Board_game_title,
-	Designer_id,
-	category_id,
-	weight, 
-	owned,
-  avg_rating,
-  min_player,
-  Max_player,
-  min_time,
-  Max_time,
-  Avg_time,
-  Num_votes,
-  Img,
-  Age
-
+	id_board_game integer,
+	Board_game_title text,
+	Designer_id integer,
+	category_id integer,
+	weight integer, 
+	num_owned integer,
+	avg_rating float,
+	min_players integer,
+	max_players integer,
+	min_time float,
+	max_time float,
+	avg_time float,
+	num_votes integer,
+	img text,
+	age integer
 );
 
 CREATE TABLE all_board_games(
-	id board_game
+	id_board_game integer
 );
 
 
 CREATE TABLE designer(
-	id designer,
-	designer_name
+	id_designer integer,
+	designer_name text
 );
 
 CREATE TABLE categories(
-	id categories,
-	categories_name
-
+	id_categories integer,
+	categories_name text
 );
 
