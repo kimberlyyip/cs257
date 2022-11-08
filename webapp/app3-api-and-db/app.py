5af933765c89901e6902be7e436f577dbfd20650
Kimberly Yip <yipk@carleton.edu>
2022-11-08 14:15:26 -0600

    moved comments

--- webapp/app3-api-and-db/app.py
+++ webapp/app3-api-and-db/app.py
@@ -13,44 +13,44 @@ app = flask.Flask(__name__, static_folder='static', template_folder='templates')
 app.register_blueprint(api.api, url_prefix='/api')
 
 
-@app.route('/') 
-'''route to home'''
+@app.route('/')
 def home():
+    '''route to home'''
     return flask.render_template('index.html')
 
-@app.route('/mockups1') 
-'''route to mockups1'''
+@app.route('/mockups1')
 def mockups1():
+    '''route to mockups1'''
     return flask.render_template('mocksup1.html')
 
 @app.route('/mocksup2') 
-'''route to mockups2'''
 def mockups2():
+    '''route to mockups2'''
     return flask.render_template('mockups2.html')
 
 @app.route('/mockups3') 
-'''route to mockups3'''
 def mockups3():
+    '''route to mockups3'''
     return flask.render_template('mockups3.html')
 
 @app.route('/mockups4') 
-'''route to mockups4'''
 def mockups4():
+    '''route to mockups4'''
     return flask.render_template('mockups4.html')
 
 @app.route('/mockups5') 
-'''route to mockups5'''
 def mockups5():
+    '''route to mockups5'''
     return flask.render_template('mocksup5.html')
 
 @app.route('/mockups6') 
-'''route to mockups6'''
 def mockups6():
+    '''route to mockups6'''
     return flask.render_template('mockups6.html')
 
 @app.route('/mockups7')
-'''route to mockups7'''
 def mockups7():
+    '''route to mockups7'''
     return flask.render_template('mockups7.html')
 
 if __name__ == '__main__':
