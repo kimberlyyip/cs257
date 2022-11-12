f4d8d988f06d4c2479ef57b7807b24ae020d5143
Kimberly Yip <yipk@carleton.edu>
2022-11-12 15:35:32 -0600

    database tables

---
+++ webapp/data/database.sql
@@ -0,0 +1,38 @@
+CREATE TABLE categories(
+    id integer,
+    category text
+);
+
+CREATE TABLE designer(
+    id integer, 
+    designer text
+);
+
+CREATE TABLE games (
+    game_id integer,
+    name text,
+    rank integer,
+    min_player integer,
+    max_player integer,
+    avg_time integer,
+    min_time integer,
+    max_time integer,
+    avg_rating float,
+    num_votes integer,
+    image_url text,
+    min_age integer,
+    num_owned integer,
+    designer_id integer,
+    pub_year integer,
+    weight float
+);
+
+CREATE TABLE game_categories(
+    game_id integer, 
+    category_id integer
+);
+
+
+
+
+
