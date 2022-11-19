d8e449acab98ecd3934024c512e6786040d1e69a
Sydney Nguyen <102560004+nguyens3@users.noreply.github.com>
2022-11-18 20:03:02 -0600

    added reviews

--- webapp/static/boardgames.js
+++ webapp/static/boardgames.js
@@ -104,13 +104,47 @@ function onGamesSelectionChanged() {
 
 
 function submit_review(){
-    review_text = document.getElementById('input').value
-    alert(review_text)
-    fetch(url, {method: 'POST', body : JSON.stringify({text: review_text})} )
-    .then((response) => response.json())
-    
+    var baseURL = getAPIBaseURL()
+    var game_name = document.getElementById('game_game_name').innerHTML;
+    var review_text = document.getElementById('input').value; 
 
-    .catch(function(error) {
-        console.log(error);
+    var url = baseURL + '/game/' + game_name + '/add/' + review_text;
+    fetch (url, {
+      method: 'POST',
+      body: JSON.stringify({
+        review_text: review_text,
+        game_name: game_name,
+      }),
+      headers: {
+        "Content-type" : "application/json; charset=UTF-8"
+      }
+    })
+    .then (response => response.json())
+
+  }
+
+function all_reviews(){
+    var baseURL = getAPIBaseURL()
+    var game_name = document.getElementById('game_game_name').innerHTML;
+    var url = baseURL + '/game/' + game_name + '/review';
+    var reference = document.getElementById('reference');
+    fetch(url,{ method: 'get'})
+    .then((response) =>response.json())
+    .then (jsondata => {
+        var games_html = '';
+        for( var i = 0; i < jsondata.length; i++){
+            review = jsondata[i]
+            games_html += '<h3>Reviews</h3> <h4>Review' + str(i+1) + '</h4>  <p>' + review + '</p>'
+        }
+   
+
+   if (reference)
+   {
+       reference.innerHTML += games_html;
+   }       
+    })
+    .catch(error => {
+   console.log(error);
     });
+
 }
