404ce7f1f58e25e1126b861ff5c80aa8308610f6
root <root@LAPTOP-QSKLA75C>
2022-11-19 19:33:28 -0600

    small adjustments

--- webapp/static/boardgames.js
+++ webapp/static/boardgames.js
@@ -25,83 +25,6 @@ function getAPIBaseURL() {
     return baseURL;
 }
 
-function loadGamesSelector() {
-    let url = getAPIBaseURL() + '/games/';
-
-    // Send the request to the books API /authors/ endpoint
-    fetch(url, {method: 'get'})
-
-    // When the results come back, transform them from a JSON string into
-    // a Javascript object (in this case, a list of author dictionaries).
-    .then((response) => response.json())
-
-    // Once you have your list of author dictionaries, use it to build
-    // an HTML table displaying the author names and lifespan.
-    .then(function(games) {
-        // Add the <option> elements to the <select> element
-        let selectorBody = '';
-        for (let k = 0; k < games.length; k++) {
-            let game = games[k];
-            selectorBody += '<option value="' + game['rank'] + '">'
-                                + game['name'] + ', ' + game['pub_year']
-                                + '</option>\n';
-        }
-
-        let selector = document.getElementById('game_selector');
-        if (selector) {
-            selector.innerHTML = selectorBody;
-        }
-    })
-
-    // Log the error if anything went wrong during the fetch.
-    .catch(function(error) {
-        console.log(error);
-    });
-}
-
-function onGamesSelectionChanged() {
-
-
-    let element = document.getElementById('game_selector');
-
-    if (!element) {
-        console.log('no element')
-        return;
-    }
-
-    let gameRank = element.value; 
-    let url = getAPIBaseURL() + '/games/' + gameRank;
-
-    console.log('gameRank:' + gameRank)
-    console.log('element:' + element)
-
-    fetch(url, {method: 'get'})
-
-    .then((response) => response.json())
-
-    .then(function(gameinfo) {
-        let tableBody = '';
-        console.log('gameinfo length:' + gameinfo.length)
-        for (let k = 0; k < gameinfo.length; k++) {
-            let game = gameinfo[k];
-            tableBody += '<tr>'
-                            + '<td>' + game['rank'] + '</td>'
-                            + '<td>' + game['name'] + '</td>'
-                            + '</tr>\n';
-        }
-
-        // Put the table body we just built inside the table that's already on the page.
-        let gameinfoTable = document.getElementById('gameinfo_table');
-        if (gameinfoTable) {
-            gameinfoTable.innerHTML = tableBody;
-        }
-    })
-
-    .catch(function(error) {
-        console.log(error);
-    });
-}
-
 
 function submit_review(){
     var baseURL = getAPIBaseURL()
@@ -134,7 +57,7 @@ function all_reviews(){
         var games_html = '';
         for( var i = 0; i < jsondata.length; i++){
             review = jsondata[i]
-            games_html += '<h3>Reviews</h3> <h4>Review' + str(i+1) + '</h4>  <p>' + review + '</p>'
+            games_html += '<h3>Reviews</h3> <h4>Review' + str(i+1) + ': </h4> <p>' + review + '</p>'
         }
    
 
